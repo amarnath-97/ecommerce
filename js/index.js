5ea1fffc20d9453e82917173d9383c8c0b508a03
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-04-17 17:48:35 +0530

    added differnt products

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
-const fetchData = async () =>{
-    const res = fetchData();
+const productcontainer = document.querySelector('.product__grid')
+
+const fetchproducts = async () => {
+    try {
+        const data = await fetch('https://fakestoreapi.com/products');
+        const value = await data.json()
+         return value 
+     } catch (error) {
+        console.error(error);
+     }
 }
+
+ const onload = async () => {
+    const value = await fetchproducts()
+    renderproducts(value)
+    
+}
+
+
+const renderproducts = (productdata) => {
+
+    productdata.forEach(element => {
+        const listItem = document.createElement('div');
+        listItem.className = 'product'
+        listItem.innerHTML = `
+  
+        <div class="prod__img__container">
+            <img src=${element.image} alt="product__img">
+        </div>
+
+        <div class="product__info">
+            <p class="product__name">${element.title} </p>
+            <p class="product__price">₹ ${element.price}</p>
+        </div>
+          <button> Add to cart     </button>
+        ` ;
+        productcontainer.appendChild(listItem);
+     });
+}
+
+
+onload()
+
+
